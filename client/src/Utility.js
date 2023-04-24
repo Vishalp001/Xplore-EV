91f03210caef2b9ffd588e29251882c0005e8426
Vishalp001 <vishalpohekar1@gmail.com>
2023-04-24 21:58:23 +0530

    Add backend api url

--- client/src/Utility.js
+++ client/src/Utility.js
@@ -1,10 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
 export const Axios = axios.create({
-  baseURL: 'https://xplor-ev.herokuapp.com/api/',
+  // baseURL: 'https://xplor-ev.herokuapp.com/api/',
+  // baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://xploreev.cyclic.app/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
     // Authorization: localStorage.getItem('ltk') || '',
   },
   maxContentLength: 20 * 1000 * 1000, // bytes => 5 MB
-})
+});
